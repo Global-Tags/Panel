0a8dfdf78899900b24dd797bbbc6f8bc01419aec
RappyTV <rappytv@rappytv.com>
2024-09-19 17:19:06 +0200

    Add component which displays 'coming soon'

---
+++ src/components/Soon.tsx
@@ -0,0 +1,7 @@
+export default function Soon() {
+    return (
+        <div className="flex flex-col justify-center items-center text-white container mx-auto px-4 py-8">
+            <h1 className="text-3xl font-bold mb-4">Coming soon...</h1>
+        </div>
+    );
+}
