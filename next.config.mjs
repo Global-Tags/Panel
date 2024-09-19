aff1a172d3bc163e5109a8a0bce58856402c1e11
RappyTV <rappytv@rappytv.com>
2024-09-19 18:15:38 +0200

    Add metrics redirect

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,11 @@ const nextConfig = {
                 source: "/github",
                 destination: "https://github.com/Global-Tags",
                 permanent: false
+            },
+            {
+                source: "/stats",
+                destination: "https://metrics.rappytv.com/public-dashboards/0f26b754dcd348a09a98ed8d24175c6e?orgId=1",
+                permanent: false
             }
         ]
     }
