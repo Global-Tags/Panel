015f036d5c354a786bde2c6f11aeba57c3f84841
RappyTV <rappytv@rappytv.com>
2024-09-22 18:13:56 +0200

    Add metadata to subpaths

--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -1,4 +1,15 @@
 import Soon from "@/components/Soon";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'Terms of Service',
+    description: 'The Terms of Service of the GlobalTags API',
+    openGraph: {
+        title: 'Terms of Service',
+        siteName: 'GlobalTags',
+        images: '/opengraph-image.png'
+    }
+}
 
 export default function Terms() {
     return Soon();
