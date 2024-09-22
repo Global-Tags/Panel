4a25d6ef233481b263c1a7178e220790499512b7
RappyTV <rappytv@rappytv.com>
2024-09-22 20:55:59 +0200

    Add image api to allowlist

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'id.rappytv.com'
+            }
+        ]  
+    },
     async redirects() {
         return [
             {
