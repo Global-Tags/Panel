e6ded64afb528c1d681875100480bd4a66797117
RappyTV <rappytv@rappytv.com>
2024-09-22 17:22:27 +0200

    Add api auth env variable

---
+++ src/app/api/lookup/[query]/route.ts
@@ -0,0 +1,28 @@
+import { config } from "@/app/config";
+import { NextRequest, NextResponse } from "next/server";
+const uuidRegex = /[a-f0-9]{8}(?:-[a-f0-9]{4}){4}[a-f0-9]{8}|[a-f0-9]{8}(?:[a-f0-9]{4}){4}[a-f0-9]{8}/;
+
+export async function GET(request: NextRequest, { params: { query } }: { params: { query: string } }) {
+    try {
+        let name, uuid = query;
+        if(!uuidRegex.test(uuid)) {
+            try {
+                const response = await fetch(`https://api.mojang.com/users/profiles/minecraft/${uuid}`);
+                const result = await response.json();
+                if(!response.ok) return new NextResponse(JSON.stringify({ error: result.errorMessage || 'Failed to retrieve uuid from username!' }), { status: response.status });
+                uuid = result.id;
+                name = result.name;
+            } catch(error: any) {
+                return new NextResponse(JSON.stringify({ error: error?.message || 'Failed to retrieve uuid from username!' }), { status: 500 });
+            }
+        }
+
+        const response = await fetch(`${config.apiUrl}/players/${uuid}`);
+        const result = await response.json();
+        if(!response.ok) return new NextResponse(JSON.stringify({ error: result.error || 'Failed to retrieve tag data!' }), { status: response.status });
+
+        return new NextResponse(JSON.stringify({ username: name, ...result }), { status: 200 });
+    } catch(error: any) {
+        return new NextResponse(JSON.stringify({ error: error?.message || 'Failed to request tag data!' }), { status: 500 });
+    }
+}
