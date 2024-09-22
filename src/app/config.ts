2cabce4f5b1037d3ad307e8ef77a8739ceab4e53
RappyTV <rappytv@rappytv.com>
2024-09-22 20:54:00 +0200

    Add team array env variable

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,5 +1,6 @@
 export const config = {
     appUrl: process.env.APP_URL!,
     apiUrl: process.env.NEXT_PUBLIC_API_BASE!,
-    apiAuth: process.env.API_AUTH!
+    apiAuth: process.env.API_AUTH!,
+    team: JSON.parse(process.env.APP_TEAM!) as { id: string, username: string, role: string }[]
 }
