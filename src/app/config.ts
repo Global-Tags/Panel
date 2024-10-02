249aa3c30bc198f60fbbe610a82388896ca554bc
RappyTV <rappytv@rappytv.com>
2024-10-02 17:22:10 +0200

    Support dynamic team page categories

--- src/app/config.ts
+++ src/app/config.ts
@@ -2,5 +2,8 @@ export const config = {
     appUrl: process.env.APP_URL!,
     apiUrl: process.env.NEXT_PUBLIC_API_BASE!,
     apiAuth: process.env.API_AUTH!,
-    team: JSON.parse(process.env.APP_TEAM!) as { id: string, username: string, category: string, role: string, joinedAt: number }[]
+    team: {
+        categories: JSON.parse(process.env.APP_TEAM_CATEGORIES!) as { id: string, name: string }[],
+        members: JSON.parse(process.env.APP_TEAM_MEMBERS!) as { id: string, username: string, category: string, role: string, joinedAt: number }[]
+    }
 }
