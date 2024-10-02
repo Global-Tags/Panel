44252a5a69f9972fd104f90e12fa38f88a06468a
RappyTV <rappytv@rappytv.com>
2024-10-02 15:01:47 +0200

    Add joinedAt and category field to member

--- src/app/config.ts
+++ src/app/config.ts
@@ -2,5 +2,5 @@ export const config = {
     appUrl: process.env.APP_URL!,
     apiUrl: process.env.NEXT_PUBLIC_API_BASE!,
     apiAuth: process.env.API_AUTH!,
-    team: JSON.parse(process.env.APP_TEAM!) as { id: string, username: string, role: string }[]
+    team: JSON.parse(process.env.APP_TEAM!) as { id: string, username: string, category: string, role: string, joinedAt: number }[]
 }
