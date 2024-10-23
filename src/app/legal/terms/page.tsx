511398a54ace51a96135d4ddae43a712c397ab16
RappyTV <rappytv@rappytv.com>
2024-10-23 19:04:35 +0200

    Fix some legal things

--- src/app/legal/terms/page.tsx
+++ src/app/legal/terms/page.tsx
@@ -15,9 +15,8 @@ export default function GeneralTermsAndConditions() {
     return (
         <div className="bg-gray-900 text-gray-300 min-h-screen flex flex-col">
             <div className="container mx-auto px-4 py-8 bg-gray-800 rounded-lg shadow-lg flex-1">
-                <h1 className="text-3xl font-bold text-blue-500 mb-6 text-center">AGB für die GlobalTags API</h1>
-                
-                <p className="mb-4"><strong>Gültig ab:</strong> 22.09.2024</p>
+                <h1 className="text-3xl font-bold text-blue-500 mb-6 text-center">Allgemeine Geschäftsbedingungen</h1>
+
                 <p className="mb-4">
                     Bei Problemen oder Fragen bitte an <Link href="mailto:contact@globaltags.xyz" className="text-blue-400 hover:underline">contact@globaltags.xyz</Link> wenden.
                 </p>
@@ -44,7 +43,7 @@ export default function GeneralTermsAndConditions() {
 
                 <h2 className="text-2xl font-semibold text-blue-500 border-b-2 border-blue-500 pb-2 mb-4">4. Datenerfassung und Datenschutz</h2>
                 <p className="mb-4">
-                    Durch die Nutzung der GlobalTags API erklären Sie sich mit der Erfassung und Nutzung von Daten gemäß der <Link href="/legal/privacy" className="text-blue-400 hover:underline">Datenschutzerklärung</Link> einverstanden. Dies umfasst die Speicherung von UUIDs, Minecraft-Versionen, Tag-Verlauf, Reports, Rollen und anderen Daten, die für die Funktionalität des Systems erforderlich sind.
+                    Zahlungen werden ausschließlich über die Monetarisierungsfunktion für sogenannte "Discord Applications" über <Link href="https://discord.com" className="text-blue-400 hover:underline" target="_blank">Discord</Link> abgewickelt. Durch die Nutzung der GlobalTags API erklären Sie sich mit der Erfassung und Nutzung von Daten gemäß der <Link href="/legal/privacy" className="text-blue-400 hover:underline">Datenschutzerklärung</Link> einverstanden. Dies umfasst die Speicherung von UUIDs, Minecraft-Versionen, Tag-Verlauf, Reports, Rollen und anderen Daten, die für die Funktionalität des Systems erforderlich sind.
                 </p>
 
                 <h2 className="text-2xl font-semibold text-blue-500 border-b-2 border-blue-500 pb-2 mb-4">5. Verwaltung und Löschung von Tags</h2>
@@ -62,11 +61,8 @@ export default function GeneralTermsAndConditions() {
                     In bestimmten Fällen können Nutzer erworbene Dienstleistungen mit eigenen Inhalten personalisieren (z. B. durch eigene Texturen). Der Nutzer versichert, dass die auf dem Server abgelegten Inhalte nicht gegen geltendes Recht verstoßen. Er verpflichtet sich, die Dienste von GlobalTags nicht zur Speicherung oder Verbreitung von obszönem, pornographischem, rassistischem, bedrohlichem oder verleumderischem Material zu verwenden. Der Nutzer darf mit seinen Dateien keinerlei Urheber-, Marken-, Patent- oder andere Rechte Dritter verletzen. Jeder Nutzer trägt die alleinige Verantwortung für seine Dateien. GlobalTags haftet weder für unmittelbare noch für mittelbare Schäden, die durch hochgeladene Dateien entstehen. Schadensersatzansprüche oder sonstige Regressansprüche gegen GlobalTags sind ausgeschlossen.
                 </p>
 
-                <h2 className="text-2xl font-semibold text-blue-500 border-b-2 border-blue-500 pb-2 mb-4">8. Datenschutz</h2>
-                <p className="mb-4">Zahlungen werden ausschließlich über die Monetarisierungsfunktion für sogenannte "Discord Applications" über <Link href="https://discord.com" className="text-blue-400 hover:underline" target="_blank">Discord</Link> abgewickelt. Des Weiteren gelten alle Datenschutzhinweise, die auf www.globaltags.xyz zu finden sind.</p>
-
-                <h2 className="text-2xl font-semibold text-blue-500 border-b-2 border-blue-500 pb-2 mb-4">9. Sonstige Bestimmungen und salvatorische Klausel</h2>
-                <p className="mb-4">Sollten einzelne Bestimmungen dieser AGB unwirksam sein, bleibt die Gültigkeit der restlichen Bestimmungen unberührt.</p>
+                <h2 className="text-2xl font-semibold text-blue-500 border-b-2 border-blue-500 pb-2 mb-4">8. Sonstige Bestimmungen und salvatorische Klausel</h2>
+                <p className="mb-4">Auf diese AGB findet ausschließlich das deutsche Recht Anwendung. Sollten einzelne Bestimmungen dieser AGB unwirksam sein, bleibt die Gültigkeit der restlichen Bestimmungen unberührt.</p>
             </div>
         </div>
     );
