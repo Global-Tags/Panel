93f7ae89dd05c7edfea6661ee76e9e16b4afb8f6
RappyTV <rappytv@rappytv.com>
2024-11-08 15:02:15 +0100

    Pass username query value to TagLookup component if available

--- src/app/lookup/page.tsx
+++ src/app/lookup/page.tsx
@@ -11,8 +11,8 @@ export const metadata: Metadata = {
     }
 }
 
-export default function Lookup() {
+export default function Lookup({ searchParams: { username }}: { searchParams: { username: string | null } }) {
     return (
-        <TagLookup />
+        <TagLookup prefetch={username} />
     );
 }
