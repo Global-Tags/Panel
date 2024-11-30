bfb44bf074bf669fadd79575e19a553df53c7a7e
RappyTV <rappytv@rappytv.com>
2024-11-30 19:15:09 +0100

    Allow all domains to serve images

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: 'id.rappytv.com'
+                hostname: '*'
             }
         ]  
     },
