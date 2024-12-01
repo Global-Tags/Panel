69c04acf8fa80d7efc937622eade12a1ccea6c26
RappyTV <rappytv@rappytv.com>
2024-12-01 20:25:36 +0100

    Set content type to json

--- src/app/api/lookup/[query]/route.ts
+++ src/app/api/lookup/[query]/route.ts
@@ -3,6 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 const uuidRegex = /[a-f0-9]{8}(?:-[a-f0-9]{4}){4}[a-f0-9]{8}|[a-f0-9]{8}(?:[a-f0-9]{4}){4}[a-f0-9]{8}/;
 
 export const revalidate = 0;
+const contentType = { 'Content-Type': 'application/json'}
 
 export async function GET(request: NextRequest, { params: { query } }: { params: { query: string } }) {
     try {
@@ -11,20 +12,20 @@ export async function GET(request: NextRequest, { params: { query } }: { params:
             try {
                 const response = await fetch(`https://api.mojang.com/users/profiles/minecraft/${uuid}`);
                 const result = await response.json();
-                if(!response.ok) return new NextResponse(JSON.stringify({ error: result.errorMessage || 'Failed to retrieve uuid from username!' }), { status: response.status });
+                if(!response.ok) return new NextResponse(JSON.stringify({ error: result.errorMessage || 'Failed to retrieve uuid from username!' }), { status: response.status, headers: contentType });
                 uuid = result.id;
                 name = result.name;
             } catch(error: any) {
-                return new NextResponse(JSON.stringify({ error: error?.message || 'Failed to retrieve uuid from username!' }), { status: 500 });
+                return new NextResponse(JSON.stringify({ error: error?.message || 'Failed to retrieve uuid from username!' }), { status: 500, headers: contentType });
             }
         }
 
         const response = await fetch(`${config.apiUrl}/players/${uuid}`, { headers: { 'Authorization': `Bearer ${config.apiAuth}` } });
         const result = await response.json();
-        if(!response.ok) return new NextResponse(JSON.stringify({ error: result.error || 'Failed to retrieve tag data!' }), { status: response.status });
+        if(!response.ok) return new NextResponse(JSON.stringify({ error: result.error || 'Failed to retrieve tag data!' }), { status: response.status, ...contentType });
 
-        return new NextResponse(JSON.stringify({ username: name, ...result }), { status: 200 });
+        return new NextResponse(JSON.stringify({ username: name, ...result }), { status: 200, headers: contentType });
     } catch(error: any) {
-        return new NextResponse(JSON.stringify({ error: error?.message || 'Failed to request tag data!' }), { status: 500 });
+        return new NextResponse(JSON.stringify({ error: error?.message || 'Failed to request tag data!' }), { status: 500, headers: contentType });
     }
 }
